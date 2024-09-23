ae4a2188ed10b6774c981fb330098ca4062307d8
lab04 <lab04@lab4-09>
2024-09-23 20:21:43 -0300

    adicionado FXML

--- cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
+++ cadastroNBA-main/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,22 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	@FXML
+	TextField txf_nome;
+	@FXML
+	TextField txf_sobrenome;
+	@FXML
+	TextField txf_idade;
+	@FXML
+	TextField txf_posicao;
+	@FXML
+	TextField txf_equipe;
+	@FXML
+	Button btn_cadastrar;
+	
 	
 }
